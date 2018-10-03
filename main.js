2af4dc7cbb296f85b9cd352cdd32c33255328cfc
Casey Gillan <gillan.casey@gmail.com>
2018-10-03 19:49:42 -0400

    Added main.js

---
+++ main.js
@@ -0,0 +1,4 @@
+const homePage = document.querySelector('.home-page');
+const portfolioPage = document.querySelector('.portfolio-page');
+const contactPage = document.querySelector('.contact-page');
+
