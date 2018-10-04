e1204b5887d931f50f260793d4dad664ea7ae490
Casey Gillan <gillan.casey@gmail.com>
2018-10-03 20:40:33 -0400

    Fixed links

--- main.js
+++ main.js
@@ -6,7 +6,10 @@ const aboutMePage = document.querySelector('.about-me-page')
 const contactLink = document.querySelector('.contact');
 const portfolioLink = document.querySelector('.portfolio');
 const aboutMeLink = document.querySelector('.about-me');
-const backLink = document.querySelector('.back');
+
+const aboutMeBackLink = document.querySelector('.about-me-back');
+const portfolioBackLink = document.querySelector('.portfolio-back');
+const contactBackLink = document.querySelector('.contact-back');
 
 aboutMeLink.addEventListener('click', function () {
   homePage.style.display = 'none';
@@ -23,9 +26,17 @@ contactLink.addEventListener('click', function () {
   contactPage.style.display = 'block';
 });
 
-backLink.addEventListener('click', function () {
+aboutMeBackLink.addEventListener('click', function () {
+  homePage.style.display = 'block';
+  aboutMePage.style.display = 'none';
+});
+
+portfolioBackLink.addEventListener('click', function () {
+  homePage.style.display = 'block';
+  portfolioPage.style.display = 'none';
+});
+
+contactBackLink.addEventListener('click', function () {
   homePage.style.display = 'block';
   contactPage.style.display = 'none';
-  portfolioPage.style.display = 'none';
-  aboutMePage.style.display = 'none';
 });
